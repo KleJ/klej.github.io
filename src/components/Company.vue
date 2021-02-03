f12ca2dee3c09c749cd8f79e9e911befc4dee88b
Mikhail Iaruntcev <mikhail.iaruntcev@t-systems.ru>
2021-02-04 01:42:09 +0300

    add experience section

---
+++ src/components/Company.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="company">
+    <span class="company__name">
+      {{ name }}
+    </span>
+    <div class="company__logo" :class="type"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Company',
+  props: ['name', 'type']
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+
+.company {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 16px;
+}
+
+.company__name {
+  padding: 5px 10px;
+  border-radius: 8px;
+  background: $blue-2;
+  line-height: 22px;
+  color: $white;
+  font-size: $font-weight-bold;
+}
+
+.company__logo {
+  position: relative;
+  left: 14px;
+  height: 22px;
+
+  &.tsys {
+    content: url('../assets/companies/tsys.png');
+  }
+
+  //&.bookingcom {
+  //  content: url('../assets/BOOKINGCOM.svg');
+  //}
+  //
+  //&.holidu {
+  //  content: url('../assets/HOLIDU.svg');
+  //}
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,61 @@
 <template>
   <div class="container">
     <div class="header">
-      <div class="header__author">{{ author }} </div>
+      <div class="header__author">{{ author }}</div>
       <div class="header__position">{{ position }}</div>
     </div>
-<!--    <div class="main">
+    <div class="main">
       <div class="main__bar">
       </div>
 
-    </div>-->
+      <h2>Profile</h2>
+      <Profile/>
+      <h2>Experience</h2>
+      <Experience
+        v-for="(work, index) in workLog" :data="work" :key="index"/>
+      <h2>Education</h2>
+      <h2>Languages</h2>
+      <h2>Skills</h2>
+    </div>
   </div>
 </template>
 
 <script>
+import Profile from "@/components/Profile";
+import Experience from "@/components/Experience";
+
 export default {
   name: 'Main',
+  components: {Experience, Profile},
   data() {
     return {
       author: 'Mikhail Iaruntcev',
-      position: 'Senior Software Engineer'
+      position: 'Senior Software Engineer',
+      workLog: [
+        {
+          title: 'Principal Frontend Developer',
+          company: 'T-Systems',
+          type: 'tsys',
+          location: 'Saint Petersburg',
+          startDate: 'May 2018',
+          endDate: 'Present',
+          desc: [
+            'Worked on the project in IoT area',
+            'Developed a data marketplace portal completely from scratch. Close collaboration with design team',
+            'Created a component base for new projects in future. Managed customer requirements and estimated task durations'
+          ],
+          stack: ['Vue.js/Vuex', 'Angular', 'Angular Material', 'TypeScript', 'RxJs', 'Webpack', 'Sass', 'Leaflet', 'Keycloack.js', 'eslint/prettier', 'Git']
+        },
+        /*{
+          title: '',
+          company: '',
+          location: '',
+          starDate: '',
+          endDate: '',
+          desc: '',
+          stack: []
+        }*/
+      ]
     };
   }
 };
@@ -41,8 +78,8 @@ export default {
 
 .header__position {
   text-align: right;
-  font-size: 16px;
-  line-height: 24px;
+  font-size: 18px;
+  line-height: 26px;
   font-weight: $font-weight-medium;
 }
 
