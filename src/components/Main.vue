9a63ab18c188d8e4bcd488aaf8802f4ae1e315b4
Mikhail Iaruntcev <mikhail.iaruntcev@t-systems.ru>
2021-02-03 23:24:20 +0300

    add title

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="container">
     <div class="header">
-      <div>{{ author }} </div>
+      <div class="header__author">{{ author }} </div>
+      <div class="header__position">{{ position }}</div>
     </div>
+<!--    <div class="main">
+      <div class="main__bar">
+      </div>
+
+    </div>-->
   </div>
 </template>
 
@@ -11,7 +17,8 @@ export default {
   name: 'Main',
   data() {
     return {
-      author: 'Mikhail Iaruntcev'
+      author: 'Mikhail Iaruntcev',
+      position: 'Senior Software Engineer'
     };
   }
 };
@@ -21,7 +28,35 @@ export default {
 @import "../styles/variables";
 
 .header {
-  color: $black;
-  margin: 64px 0;
+  color: $gold-96;
+  margin: 36px 0 72px;
+}
+
+.header__author {
+  font-size: 32px;
+  line-height: 32px;
+  font-weight: $font-weight-semi;
+  border-bottom: solid 2px;
+}
+
+.header__position {
+  text-align: right;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: $font-weight-medium;
+}
+
+.main {
+  padding: 40px 30px;
+  border-radius: 6px;
+  background: $white;
+}
+
+.main__bar {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  line-height: 45px;
+  margin-bottom: 40px;
 }
 </style>
