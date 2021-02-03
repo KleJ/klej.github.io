6276302dd3731ca3c4423198d9018445d89ba88a
Mikhail Iaruntcev <mikhail.iaruntcev@t-systems.ru>
2021-02-03 22:58:08 +0300

    add container

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="cv">
-    <div>Start page</div>
+  <div class="container">
+    <div class="header">
+      <div>{{ author }} </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {
+      author: 'Mikhail Iaruntcev'
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+
+.header {
+  color: $black;
+  margin: 64px 0;
+}
+</style>
