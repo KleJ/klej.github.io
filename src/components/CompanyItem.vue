76291106e373db6e30cb1e1d0a7d32830b80c4d2
Mikhail Iaruntcev <iaruntcev@gmail.com>
2023-03-14 02:06:02 +0400

    update image link in CompanyItem.vue

--- src/components/CompanyItem.vue
+++ src/components/CompanyItem.vue
@@ -8,14 +8,20 @@
         target="_blank"
         class="company__logo">
       <img
+          ref="image"
           class="company__logo--img"
           :src="imagePath"
           :alt="company.name"/>
+      <svg :width="width" height="32" xmlns="http://www.w3.org/2000/svg">
+        <rect x='0' y='0' fill='none' :width="width" height='32'/>
+      </svg>
     </a>
   </div>
 </template>
 
 <script>
+const FRAME = 10;
+
 export default {
   name: 'CompanyItem',
   props: ['company'],
@@ -23,6 +29,16 @@ export default {
     imagePath() {
       return require('@/assets/companies/' + this.company.type + '.png');
     }
+  },
+  data() {
+    return {
+      width: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.width = this.$refs.image.clientWidth + FRAME;
+    }, 100);
   }
 };
 </script>
@@ -36,6 +52,21 @@ export default {
   align-items: center;
   margin-bottom: 16px;
   line-height: 22px;
+
+  a {
+    position: relative;
+  }
+}
+
+svg {
+  position: absolute;
+  top: -5px;
+  left: -5px;
+}
+
+svg rect {
+  transition: all 0.8s ease-in-out;
+  stroke-dasharray: 0 500;
 }
 
 .company__name {
@@ -49,11 +80,11 @@ export default {
 .company__logo {
   margin-left: 14px;
   height: 22px;
+  transition: 1s;
 
   .company__logo--img {
     max-height: 22px;
     vertical-align: middle;
-    transition: 1s;
   }
 }
 
@@ -64,8 +95,14 @@ export default {
 }
 
 @media screen and (min-width: 600px) {
-  .company__logo--img:hover {
-    transform: scale(1.3) translateX(10px);
+  .company__logo:hover {
+    transform: scale(1.3) translateX(15px);
+
+    svg rect {
+      stroke: $blue-2;
+      stroke-width: 3;
+      stroke-dasharray: 500 0;
+    }
   }
 }
 </style>
